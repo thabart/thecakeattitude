03b11e9f24077b0893ac8d86bec6ad7020f4ad68
thabart <habarthierry@hotmail.fr>
2017-03-28 20:58:37 +0200

    Ticket 10 : Add game state (continue to work on it ;))

--- web/public/states/Splash.js
+++ web/public/states/Splash.js
@@ -4,9 +4,14 @@ Splash.prototype = {
 		this.loadingBar = this.game.make.text(this.game.world.centerX, 380, 'Loading...', {fill: 'white'});
 	},
 	preload: function() {		
+		this.game.load.script('splash', 'public/states/Game.js');
 		this.game.add.existing(this.loadingBar);
 	},
 	create: function() {
-		
+		var self = this;
+		this.game.state.add('Game', Game);
+		 setTimeout(function () {
+		  self.game.state.start("Game");
+		}, 1000);
 	}
 };
