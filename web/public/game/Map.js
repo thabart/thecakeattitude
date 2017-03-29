5e2cf243d0d26e174caa757ba970d71b2115b827
thabart <habarthierry@hotmail.fr>
2017-03-29 17:26:45 +0200

    Ticket 10 : Start refactoring the solution "add different states"

---
+++ web/public/game/Map.js
@@ -0,0 +1,54 @@
+var Map = function(tileMap, game, player) {
+	this.tileMap = tileMap;		
+	this.wraps = [];
+	this.player = player;
+	this.players = [];
+	this.layers = {
+		alimentation : null,
+		earth : null,
+		ground: null,
+		collision : null					
+	};
+	
+	this.init = function() {
+		// TODO : Load all the IMAGES.
+		this.tileMap.addTilesetImage('tallgrass', 'tallgrass');
+		this.tileMap.addTilesetImage('farming_fishing', 'farming_fishing');
+		this.tileMap.addTilesetImage('tiles', 'tiles');
+		// Add layers.					
+		this.layers.collision = this.tileMap.createLayer('Collision');
+		this.layers.ground = this.tileMap.createLayer('Ground');
+		this.layers.earth = this.tileMap.createLayer('Earth');
+		this.layers.alimentation = this.tileMap.createLayer('Alimentations');
+		this.tileMap.createLayer('Wraps');
+		// Load the WRAPS.
+		this.wraps = game.add.group();
+		this.wraps.enableBody = true;
+		this.tileMap.createFromObjects('Wraps', 421, 'wrap', 0, true, false, this.wraps);		
+		// ADD SPRITE : 
+		// this.wraps.add('tiles'); => sprite
+		// Specify which tile can collide.
+		this.tileMap.setCollision(421, true, 'Collision');
+		// Add the player to the world.
+		this.player = new Player(null, game.add.sprite(0, 0, 'player'));
+		game.physics.arcade.enable(this.player.sprite);			
+		// Make the camera follow the sprite.
+		game.camera.follow(this.player.sprite, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);	
+		// Display layer.
+		this.layers.earth.resizeWorld();
+	};
+	
+	this.destroy = function() {
+		// Destroy the layers & objects.
+		this.layers.collision.destroy();
+		this.layers.ground.destroy();
+		this.layers.earth.destroy();
+		this.layers.alimentation.destroy();
+		this.wraps.destroy();
+		this.tileMap.destroy();
+		this.player.remove();
+		this.players.forEach(function(p) {
+			p.remove();
+		});
+	};
+};
---
+++ web/public/game/Player.js
@@ -0,0 +1,90 @@
+var Player = function(id, sprite) {
+	this.id = id;
+	this.sprite = sprite;
+	this.direction = [];
+	this.message = null;
+	this.evtMessage = null;
+	
+	function updateDirection(dir, val) {
+		var result = false;
+		if (dir.isDown && this.direction.indexOf(val) == -1) {
+			this.direction.push(val);
+			result = true;
+		} else if (dir.isUp && this.direction.indexOf(val) != -1) {
+			this.direction.splice(this.direction.indexOf(val), 1);		
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	// Remove the player.
+	this.remove = function() {
+		this.sprite.destroy();
+		if (this.message != null)
+		{					
+			this.message.destroy();
+		}
+	};				
+	// Update direction.
+	// Returns : true if the direction changes.
+	this.updateDirection = function(cursors) {
+		var result = false;
+		result = result || updateDirection.call(this, cursors.up, 0);
+		result = result || updateDirection.call(this, cursors.right, 1);
+		result = result || updateDirection.call(this, cursors.down, 2);
+		result = result || updateDirection.call(this, cursors.left, 3);
+		return result;
+	};				
+	// Update the properties.
+	this.update = function(x, y, direction) {
+		this.sprite.x = x;
+		this.sprite.y = y;
+		this.direction = direction;
+	};				
+	// Display message
+	this.displayMessage = function(message, evtMessage) {
+		this.message = message;
+		this.evtMessage = evtMessage;
+	};				
+	// Destroy the message.
+	this.destroyMessage = function() {
+		if (this.message == null) {
+			return;
+		}
+		
+		this.message.destroy();
+		this.message = null;
+		this.evtMessage = null;					
+	};				
+	// Update player position.
+	this.updatePosition = function() {					
+		this.sprite.body.velocity.x = 0;
+		this.sprite.body.velocity.y = 0;
+		if (this.direction.indexOf(0) != -1) {						
+			this.sprite.body.velocity.y = -200;
+		} else if (this.direction.indexOf(2) != -1) {			
+			this.sprite.body.velocity.y = 200;						
+		}
+		
+		if (this.direction.indexOf(1) != -1) {	
+			this.sprite.body.velocity.x = 200;
+		} else if (this.direction.indexOf(3) != -1) {			
+			this.sprite.body.velocity.x = -200;						
+		}
+	};				
+	// Update message position.
+	this.updateMessagePosition = function() {
+		if (this.message == null) {
+			return;
+		}
+		
+		this.message.x = Math.floor(this.sprite.x + (this.sprite.width / 2));
+		this.message.y = Math.floor(this.sprite.y);				
+	};				
+	// Reset the direction.
+	this.resetDirection = function() {
+		this.direction = [];
+	};
+}
+		
