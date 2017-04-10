71cd5e6ffa2d8a91a1a7c2cbef7a3cf055fc2a12
thabart <habarthierry@hotmail.fr>
2017-04-10 21:13:17 +0200

    Ticket 6 : Update user information

--- game/public/game/Constants.js
+++ game/public/game/Constants.js
@@ -4,5 +4,6 @@ var Constants = {
 	ClientId : 'game',
 	ClientSecret: 'game',
 	openIdWellKnownConfiguration: 'http://localhost:5001/.well-known/openid-configuration',
-	userClaims : 'http://localhost:5001/users/claims'
+	userClaims : 'http://localhost:5001/users/claims',
+	googleMapUrl: 'http://maps.googleapis.com/maps/api'
 };
--- game/public/states/Menu.js
+++ game/public/states/Menu.js
@@ -2,22 +2,7 @@
 var Menu = function() {};
 Menu.prototype = {
 	preload: function() { },
-	init: function() {		
-		var buildModal = function() {
-			var result = $("<div class='modal fade'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'>"+
-			"<h5 class='modal-title'>Client</h5>"+
-			"<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+
-			"</div>"+
-			"<div class='modal-body'>"+
-			"<div class='form-group'><label>Pseudo</label><input type='text' class='form-control' id='pseudo'/></div>"+
-			"</div>"+
-			"<div class='modal-footer'>"+
-			"<button type='button' class='btn btn-success' id='confirm'>Confirm</button>"+
-			"<button type='button' class='btn btn-secondary' data-dismiss='modal'>Cancel</button>"+
-			"</div></div></div></div>");			
-			$(document.body).append(result);
-			return result;
-		};		
+	init: function(subject) {	
 		var buildSettingsModal = function() {
 			var result = $("<div class='modal fade'><div class='modal-dialog'><div class='modal-content'>"+
 				"<div class='modal-header'><h5 class='modal-title'>Settings</h5><button type='button' class='close' data-dismiss='modal'><span >&times;</span></button></div>"+
@@ -37,7 +22,8 @@ Menu.prototype = {
 					"<div class='tab-pane' id='contactinformation'>"+
 						"<div class='form-group'><label class='control-label'>Street Address</label><input type='text' class='form-control' name='street_address' /></div>"+
 						"<div class='form-group'><label class='control-label'>Postal code</label><input type='text' class='form-control' name='postal_code' /></div>"+	
-						"<div class='form-group'><label class='control-label'>City</label><input type='text' class='form-control' name='country' /></div>"+
+						"<div class='form-group'><label class='control-label'>Locality</label><input type='text' class='form-control' name='locality' /></div>"+
+						"<div class='form-group'><label class='control-label'>Country</label><input type='text' class='form-control' name='country' /></div>"+
 						"<div class='form-group'><input type='checkbox' /> Use current location</div>"+
 					"</div>"+
 				"</div></div></div><div style='width:100%; text-align: center;' id='loading'><i class='fa fa-spinner fa-spin' style='font-size:24px; width: 24px; height:24px;'></i></div>"+
@@ -75,12 +61,70 @@ Menu.prototype = {
 					return result;
 				}
 			},
+			getAddress = function(adrComponents) {
+				var result = {};
+				var streetNumber = "",
+					route = "";
+				adrComponents.forEach(function(adrComponent) {		
+					if (!adrComponent.types) {
+						return;
+					}
+					
+					if (adrComponent.types.includes("street_number")) {
+						streetNumber = adrComponent.long_name;
+						return;
+					}
+					
+					if (adrComponent.types.includes("route")) {
+						route = adrComponent.long_name;
+						return;
+					}
+				
+					if (adrComponent.types.includes("postal_code")) {
+						result['postal_code'] = adrComponent.long_name;
+						return;
+					}
+					
+					if (adrComponent.types.includes("locality")) {
+						result['locality'] = adrComponent.long_name;
+						return;
+					}
+
+					if (adrComponent.types.includes("country")) {
+						console.log(adrComponent);
+						result['country'] = adrComponent.long_name;
+						return;
+					}
+				});
+				
+				result["street_address"] = streetNumber +" "+route;
+				console.log(result);
+				return result;
+			},
 			self = this,
+			displayAddress = function(adr) {				
+				$(settingsModal).find("input[name='street_address']").val(adr.street_address);
+				$(settingsModal).find("input[name='postal_code']").val(adr.postal_code);
+				$(settingsModal).find("input[name='locality']").val(adr.locality);
+				$(settingsModal).find("input[name='country']").val(adr.country);
+			},
 			sessionName = Constants.SessionName,
-			modal = buildModal(),
 			accessToken = sessionStorage.getItem(sessionName),
 			getFormData = Helpers.getFormData,
 			settingsModal = buildSettingsModal(),
+			retrieUserInformation = function(successCb, errorCb) {
+				$.ajax(Constants.userClaims, {
+					type: 'GET',
+					headers: {
+						'Authorization': 'Bearer '+accessToken
+					}
+				}).then(function(userInfo) {	
+					if (successCb) successCb(userInfo);
+				}).fail(function(e, m) {
+					errorModal.display('User information cannot be retrieved', 3000, 'error');		
+					if (errorCb) errorCb();
+				});				
+			},
 			displaySettingsLoading = function(isLoading) {
 				if (isLoading) {				
 					$(settingsModal).find('#loading').show();
@@ -93,27 +137,18 @@ Menu.prototype = {
 			},
 			displaySettings = function() {
 				displaySettingsLoading(true);
-				$.ajax(Constants.userClaims, {
-					type: 'GET',
-					headers: {
-						'Authorization': 'Bearer '+accessToken
-					}
-				}).then(function(userInfo) {	
+				retrieUserInformation(function(userInfo) {
 					$(settingsModal).find("input[name='name']").val(userInfo.name);
 					$(settingsModal).find("input[name='phone_number']").val(userInfo.phone_number);
 					$(settingsModal).find("input[name='email']").val(userInfo.email);
 					if (userInfo.address) {
 						var adr = parseAddress(userInfo.address);	
-						$(settingsModal).find("input[name='street_address']").val(adr.street_address);
-						$(settingsModal).find("input[name='postal_code']").val(adr.postal_code);
-						$(settingsModal).find("input[name='country']").val(adr.country);
+						displayAddress(adr);
 					}
 					
+					displaySettingsLoading(false);					
+				}, function() {					
 					displaySettingsLoading(false);
-				}).fail(function(e, m) {
-					console.log(e);
-					displaySettingsLoading(false);
-					errorModal.display('User information cannot be retrieved', 3000, 'error');		
 				});
 			},
 			optStyle = { font: '20px Arial', fill: '#ffffff' },
@@ -124,17 +159,6 @@ Menu.prototype = {
 			return;
 		}
 		
-		$(modal).find('#confirm').click(function() {
-			var newPlayer = {
-				pseudo : $(modal).find('#pseudo').val(),
-				map: 'firstMap'
-			};
-
-			$(modal).modal('hide');			
-			$(settingsModal).remove();
-			$(modal).remove();
-			self.game.state.start("Game", true, false, newPlayer);
-		});
 		$(settingsModal).find('#update-profile').submit(function(e) {
 			e.preventDefault();
 			displaySettingsLoading(true);
@@ -147,31 +171,64 @@ Menu.prototype = {
 					'Authorization': 'Bearer '+accessToken
 				}				
 			}).then(function() {
-				displaySettings();
+				$(settingsModal).modal('toggle');
+				errorModal.display('Information has been updated', 3000, 'success');						
 			}).fail(function(e, m) {
 				errorModal.display('Error while trying to update the user information', 3000, 'error');		
 				displaySettingsLoading(false);				
 			});
 		});
-		self.game.add.text(titlePaddingTop, titlePaddingLeft, '<title>', titleStyle);
+		$(settingsModal).find("input[type='checkbox']").click(function() {
+			var isChecked = $(this).is(':checked');
+			if (!navigator.geolocation) {
+				errorModal.display('the geolocation is not supported by this browser', 3000, 'error');
+				return;
+			}
+			
+			displaySettingsLoading(true);
+			navigator.geolocation.getCurrentPosition(function(position) {
+				var url = Constants.googleMapUrl + "/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude;
+				$.get(url).then(function(r) {					
+					displaySettingsLoading(false);
+					if (!r || !r.results || r.results.length == 0) {
+						return;
+					}
+					
+					var adrComponent = r.results[0].address_components;
+					var adr = getAddress(adrComponent);	
+					displayAddress(adr);
+				})
+				.fail(function() {
+					errorModal.display('Google map cannot be reached', 3000, 'error');						
+					displaySettingsLoading(false);	
+				});
+			});
+		});
 		var menu = [
 			{
-				title: 'Seller',
+				title: 'Shops',
 				spriteName: 'shopper',
 				width: shopperWidth,
 				height: shopperHeight,
 				callback: function() {
 					$(settingsModal).remove();
-					$(modal).remove();
 					self.game.state.start("CharacterChooser");
 				}
 			}, {
-				title: 'Shopper',
+				title: 'Connect',
 				spriteName: 'seller',
 				width: sellerWidth,
 				height: sellerHeight,
 				callback: function() {
-					$(modal).modal('toggle');
+					retrieUserInformation(function(userInfo) {						
+						var newPlayer = {
+							pseudo : userInfo.name,
+							map: 'firstMap'
+						};
+
+						$(settingsModal).remove();
+						self.game.state.start("Game", true, false, newPlayer);
+					});
 				}
 			}, {
 				title: 'Settings',
