85b70ec9cc73d6528a0f942e4526769efab56438
thabart <habarthierry@hotmail.fr>
2017-04-18 19:19:06 +0200

    Ticket 4 : Add google map

--- web/src/Map.js
+++ web/src/Map.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import TrendingSellers from './widgets/trendingSellers';
 import BestDeals from './widgets/bestDeals';
 import PublicAnnouncements from './widgets/publicAnnouncements';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 import './Map.css';
 import $ from 'jquery';
 import 'jquery-ui/ui/widgets/sortable';
+
+const GettingStartedGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={3}
+    defaultCenter={{ lat: -25.363882, lng: 131.044922 }}>
+  </GoogleMap>
+));
 
 class Map extends Component {
   render() {
@@ -16,7 +24,16 @@ class Map extends Component {
   			</form>
         <div className="row">
           <div className="col-md-6">
-            <div id="map" className="col-md-12"></div>
+            <div id="map" className="col-md-12">
+            <GettingStartedGoogleMap
+                  containerElement={
+                    <div style={{ height: `100%` }} />
+                  }
+                  mapElement={
+                    <div style={{ height: `100%` }} />
+                  }
+                />
+            </div>
           </div>
           <div className="col-md-6">
             <ul className="row list-unstyled" ref={(elt) => {this.widgetContainer = elt; }}>
