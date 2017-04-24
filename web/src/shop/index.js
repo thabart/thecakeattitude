d2d8e7d7fc2c95d943fc3b6e8d13622124307d28
thabart <habarthierry@hotmail.fr>
2017-04-24 11:45:37 +0200

    Ticket 4 : Move address to another component

--- web/src/shop/index.js
+++ web/src/shop/index.js
@@ -1,7 +1,9 @@
 import ContactInfoForm from './contactInfoForm';
 import DescriptionForm from './descriptionForm';
 import PaymentForm from './paymentForm';
+import AddressForm from './addressForm';
 
 exports.ContactInfoForm = ContactInfoForm;
 exports.DescriptionForm = DescriptionForm;
 exports.PaymentForm = PaymentForm;
+exports.AddressForm = AddressForm;
