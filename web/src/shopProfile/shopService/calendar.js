f7af0d5a527669ee03fd106ec95bcb90ec5ea453
thabart <habarthierry@hotmail.fr>
2017-05-10 16:26:42 +0200

    Ticket 25 : Display big calendar

--- web/src/shopProfile/shopService/calendar.js
+++ web/src/shopProfile/shopService/calendar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './calendar.css';
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
+
+var myEvts = [
+  {
+   'title': 'All Day Event',
+   'start': new Date(2017, 4, 10, 17, 0, 0, 0),
+   'end': new Date(2017, 4, 10, 17, 30, 0, 0),
+   'desc': 'IMPORTANT'
+ }
+];
 
 class Calendar extends Component {
-  // Return the view.
   render() {
-    return (<div>
-
+    console.log(myEvts);
+    return (<div className="shop-service-calendar">
+      <BigCalendar
+            events={myEvts}
+          />
       </div>);
   }
 }
