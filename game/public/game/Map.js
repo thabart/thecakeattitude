5a83f6ff474ff26a3356f9a634e99de54b203c67
thabart <habarthierry@hotmail.fr>
2017-06-02 14:27:13 +0200

    Ticket 10 : Start to update the game

--- game/public/game/Map.js
+++ game/public/game/Map.js
@@ -4,37 +4,58 @@ var Map = function(key, tileMap, game, category) {
 	this.npcs = [];
 	this.npcObjs = null;
 	this.warps = null;
+	this.playersGroup = null;
 	this.key = key;
-	this.tileMap = tileMap;		
+	this.tileMap = tileMap;
 	this.player = null;
 	this.players = [];
 	this.warpRectangles = [];
 	this.layers = {
-		alimentation : null,
-		earth : null,
+		collision : null,
 		ground: null,
-		collision : null					
+		groundDecorations: null,
+		shadows : null,
+		houses1: null,
+		houses2: null,
+		decorations: null,
+		walls: null
+	},
+	this.groups = {
+		players: null
 	};
-	
-	this.init = function() {	
-		var self = this;		
+
+	this.init = function() {
+		var self = this;
 		var deferredLoaded = [];
-		// TODO : Load all the IMAGES.
+		// Load the images.
+		this.tileMap.addTilesetImage('Town@64x64', 'Town@64x64');
+		this.tileMap.addTilesetImage('Shadows@64x64', 'Shadows@64x64');
+		this.tileMap.addTilesetImage('tiles', 'tiles');
+
+		/*
 		this.tileMap.addTilesetImage('tallgrass', 'tallgrass');
 		this.tileMap.addTilesetImage('farming_fishing', 'farming_fishing');
-		this.tileMap.addTilesetImage('tiles', 'tiles');
 		this.tileMap.addTilesetImage('plowed_soil', 'plowed_soil');
 		this.tileMap.addTilesetImage('house', 'house');
 		this.tileMap.addTilesetImage('freePlace', 'freePlace');
 		this.tileMap.addTilesetImage('floor', 'floor');
 		this.tileMap.addTilesetImage('stuff', 'stuff');
 		this.tileMap.addTilesetImage('panel-info', 'panel-info');
-		// Add layers.					
+		*/
+
+		// Add layers.
 		this.layers.collision = this.tileMap.createLayer('Collision');
 		this.layers.ground = this.tileMap.createLayer('Ground');
-		this.layers.earth = this.tileMap.createLayer('Earth');
-		this.layers.alimentation = this.tileMap.createLayer('Alimentations');
+		this.layers.groundDecorations = this.tileMap.createLayer('GroundDecorations');
+		this.groups.players = game.add.group();
+		this.layers.shadows = this.tileMap.createLayer('Shadows');
+		this.layers.houses1 = this.tileMap.createLayer('Houses1');
+		this.layers.houses2 = this.tileMap.createLayer('Houses2');
+		this.layers.decorations = this.tileMap.createLayer('Decorations');
+		this.layers.walls = this.tileMap.createLayer('Walls');
 		this.tileMap.createLayer('Wraps');
+
+		/*
 		// Create all the objects.
 		this.warps = game.add.group();
 		this.npcObjs = game.add.group();
@@ -58,15 +79,15 @@ var Map = function(key, tileMap, game, category) {
 								promise.resolve();
 								return;
 							}
-														
+
 							self.npcObjs.add(obj.panelInfo.getSprite());
-							self.npcs.push([obj.panelInfo.getSprite(), obj.panelInfo ]);							
+							self.npcs.push([obj.panelInfo.getSprite(), obj.panelInfo ]);
 							self.warps.add(obj.warp);
 							self.warps.set(obj.warp, 'map', "shop_" + obj.map_id, false, false, 0, true);
 							self.warps.set(obj.warp, 'warp_entry', 'warp', false, false, 0, true);
 							self.warps.set(obj.warp, 'entry_dir', 'bottom', false, false, 0, true);
 							self.warps.set(obj.warp, 'category', category, false, false, 0, true);
-							promise.resolve();					
+							promise.resolve();
 						});
 						deferredLoaded.push(promise);
 					break;
@@ -74,21 +95,21 @@ var Map = function(key, tileMap, game, category) {
 						o = new StockManager(game, npc);
 					break;
 				}
-				
+
 				if (o == null) {
 					return;
 				}
-				
+
 				var sp = o.getSprite();
 				self.npcObjs.add(sp);
 				self.npcs.push([sp, o ]);
 			});
 		}
-		
+
 		// Create warps.
 		if (wps) {
-			wps.forEach(function(warp) {				
-				// Display warp.				
+			wps.forEach(function(warp) {
+				// Display warp.
 				var warpRectangle = game.add.graphics(0, 0);
 				warpRectangle.lineStyle(2, 0xd9d9d9, 1);
 				warpRectangle.beginFill(0xFFFFFF, 1);
@@ -104,32 +125,32 @@ var Map = function(key, tileMap, game, category) {
 				{
 					self.warps.set(rect, property, warp.properties[property], false, false, 0, true);
 				}
-				
+
 				self.warps.set(rect, 'category', category, false, false, 0, true);
 			});
 		}
-		
+		*/
+
 		// Specify which tile can collide.
-		this.tileMap.setCollision(3, true, 'Collision');		
+		this.tileMap.setCollision(3, true, 'Collision');
 		var result = $.Deferred();
 		$.when.apply(null, deferredLoaded).done(function() {
 			result.resolve();
 		});
-		
+
 		return result.promise();
 	};
-	
-	this.addPlayer = function(playerX, playerY, pseudo) {	
-		this.layers.earth.resizeWorld();
-        game.world.setBounds(0, 0, game.world.width, game.world.height);
-		this.player = new Player(null, playerX, playerY, game, true, pseudo);
+
+	this.addPlayer = function(playerX, playerY, pseudo) {
+		this.layers.ground.resizeWorld();
+    game.world.setBounds(0, 0, game.world.width, game.world.height);
+		this.player = new Player(null, playerX, playerY, game, true, pseudo); // Set current player.
 		this.player.sprite.body.collideWorldBounds = true;
-		// Make the camera follow the sprite.
-		// game.camera.follow(this.player.sprite);
+		this.groups.players.add(this.player.sprite);
 		game.camera.focusOnXY(playerX, playerY);
 		game.camera.follow(this.player.sprite, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 	};
-	
+
 	this.display = function(isVisible) {
 		if (this.layers.collision) this.layers.collision.visible = isVisible;
 		if (this.layers.ground) this.layers.ground.visible = isVisible;
@@ -146,14 +167,14 @@ var Map = function(key, tileMap, game, category) {
 			p.display(isVisible);
 		});
 	};
-	
+
 	this.destroy = function() {
 		// Destroy the layers
 		if (this.layers.collision) this.layers.collision.destroy();
 		if (this.layers.ground) this.layers.ground.destroy();
 		if (this.layers.earth) this.layers.earth.destroy();
 		if (this.layers.alimentation) this.layers.alimentation.destroy();
-		
+
 		// Destroy objects
 		this.npcs.forEach(function(npc) {
 			npc[1].destroy();
@@ -170,11 +191,11 @@ var Map = function(key, tileMap, game, category) {
 		if (this.tileMap) this.tileMap.destroy();
 		if (this.player) this.player.remove();
 	};
-	
+
 	this.getNpcObjs = function() {
 		return this.npcObjs;
 	};
-	
+
 	this.getNpc = function(sprite) {
 		var result = null;
 		this.npcs.forEach(function(kvp) {
@@ -182,10 +203,10 @@ var Map = function(key, tileMap, game, category) {
 				result = kvp[1];
 			}
 		});
-		
+
 		return result;
 	};
-	
+
 	this.getWarps = function() {
 		return this.warps;
 	};
--- game/public/game/Player.js
+++ game/public/game/Player.js
@@ -12,30 +12,30 @@ var Player = function(id, x, y, game, currentUser, pseudo) {
 	interactionSprite.fixedToCamera = true;
 	interactionSprite.visible = false;
 	this.sprite.addChild(this.hitZone);
-	
+
 	// TODO : MAX : Characters in pseudo = 10
 	var pseudoStyle = { font: '15px', wordWrap: true, wordWrapWidth: 60, align: 'center' };
 	if (currentUser) {
 		pseudoStyle.fill = '#FFA500';
 	}
-	
+
 	this.pseudo = game.add.text(-this.sprite.width / 2, -15, pseudo, pseudoStyle);
 	this.sprite.addChild(this.pseudo);
 	game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
-	
+
 	function updateDirection(dir, val) {
 		var result = false;
 		if (dir.isDown && this.direction.indexOf(val) == -1) {
 			this.direction.push(val);
 			result = true;
 		} else if (dir.isUp && this.direction.indexOf(val) != -1) {
-			this.direction.splice(this.direction.indexOf(val), 1);		
+			this.direction.splice(this.direction.indexOf(val), 1);
 			result = true;
 		}
-		
+
 		return result;
 	}
-	
+
 	// Remove the player.
 	this.remove = function() {
 		this.sprite.destroy();
@@ -43,7 +43,7 @@ var Player = function(id, x, y, game, currentUser, pseudo) {
 		this.pseudo.destroy();
 		interactionSprite.destroy();
 		if (this.message != null)
-		{					
+		{
 			this.message.destroy();
 		}
 	};
@@ -52,9 +52,9 @@ var Player = function(id, x, y, game, currentUser, pseudo) {
 		if (this.message == null) {
 			return;
 		}
-		
+
 		this.message.x = Math.floor(this.sprite.x + (this.sprite.width / 2));
-		this.message.y = Math.floor(this.sprite.y);				
+		this.message.y = Math.floor(this.sprite.y);
 	};
 	this.display = function(isVisible) {
 		if (this.sprite) this.sprite.visible = isVisible;
@@ -70,44 +70,44 @@ var Player = function(id, x, y, game, currentUser, pseudo) {
 		result = result || updateDirection.call(this, cursors.down, 2);
 		result = result || updateDirection.call(this, cursors.left, 3);
 		return result;
-	};		
+	};
 	// Update the properties.
 	this.update = function(x, y, direction) {
 		this.sprite.x = x;
 		this.sprite.y = y;
 		this.direction = direction;
-	};				
+	};
 	// Display message
 	this.displayMessage = function(message, evtMessage) {
 		this.message = message;
 		this.evtMessage = evtMessage;
-	};				
+	};
 	// Destroy the message.
 	this.destroyMessage = function() {
 		if (this.message == null) {
 			return;
 		}
-		
+
 		this.message.destroy();
 		this.message = null;
-		this.evtMessage = null;					
-	};				
+		this.evtMessage = null;
+	};
 	// Update player position.
 	this.updatePosition = function() {
 		this.sprite.body.velocity.x = 0;
 		this.sprite.body.velocity.y = 0;
-		if (this.direction.indexOf(0) != -1) {						
-			this.sprite.body.velocity.y = -200;
-		} else if (this.direction.indexOf(2) != -1) {			
-			this.sprite.body.velocity.y = 200;						
+		if (this.direction.indexOf(0) != -1) {
+			this.sprite.body.velocity.y = -300;
+		} else if (this.direction.indexOf(2) != -1) {
+			this.sprite.body.velocity.y = 300;
 		}
-		
-		if (this.direction.indexOf(1) != -1) {	
-			this.sprite.body.velocity.x = 200;
-		} else if (this.direction.indexOf(3) != -1) {			
-			this.sprite.body.velocity.x = -200;						
+
+		if (this.direction.indexOf(1) != -1) {
+			this.sprite.body.velocity.x = 300;
+		} else if (this.direction.indexOf(3) != -1) {
+			this.sprite.body.velocity.x = -300;
 		}
-	};	
+	};
 	// Reset the direction.
 	this.resetDirection = function() {
 		this.direction = [];
@@ -138,7 +138,6 @@ var Player = function(id, x, y, game, currentUser, pseudo) {
 	};
 	// Hide interaction button.
 	this.hideInteraction = function() {
-		 interactionSprite.visible = false;		
+		 interactionSprite.visible = false;
 	};
 }
-		
