c9b58ec21ca8971d533817964ac74b196d81a111
thabart <habarthierry@hotmail.fr>
2017-06-13 19:11:14 +0200

    Ticket 10 : Display payment methods

--- game/public/modals/freePlaceModalTabs/freePlaceModalPaymentTab.js
+++ game/public/modals/freePlaceModalTabs/freePlaceModalPaymentTab.js
@@ -3,23 +3,49 @@ FreePlaceModalPaymentTab.prototype = {
   render: function() {
     var confirm = $.i18n('confirm'),
     previous = $.i18n('previous'),
+    cash = $.i18n('cash'),
+    paypal = $.i18n('paypal'),
+    bankTransfer = $.i18n('bankTransfer'),
+    insertIban = $.i18n('insertIban'),
+    chooseOneOreMorePaymentMethods = $.i18n('chooseOneOreMorePaymentMethods'),
     self = this;
-    var result = $("<div class='container'>"+
+    self.tab = $("<div class='container'>"+
+      "<p><i class='fa fa-exclamation-circle' /> "+chooseOneOreMorePaymentMethods+"</p>"+
       "<div>"+
-        "<section class='col-6'>"+
-        "</section>"+
+        "<ul class='list-group payments'>"+
+          "<li class='list-group-item'><div class='checkbox-container'><i class='fa fa-check checkbox' /></div><i class='fa fa-money' /> "+cash+"</li>"+
+          "<li class='list-group-item'>"+
+            "<div class='checkbox-container'><i class='fa fa-check checkbox' /></div>"+
+            "<i class='fa fa-credit-card-alt' /> "+bankTransfer+
+            "<p>"+insertIban+"</p>"+
+            "<div>"+
+              "<input type='text' class='input-control col-3 iban' maxLength='4' placeholder='BE__' name='firstIban' />"+
+              "<input type='text' class='input-control col-3 iban' maxLength='4' placeholder='0000' name='secondIban' />"+
+              "<input type='text' class='input-control col-3 iban' maxLength='4' placeholder='0000' name='thirdIban' />"+
+              "<input type='text' class='input-control col-3 iban' maxLength='4' placeholder='0000' name='fourthIban' />"+
+            "</div>"+
+          "</li>"+
+          "<li class='list-group-item'><div class='checkbox-container'><i class='fa fa-check checkbox' /></div><i class='fa fa-paypal' /> "+bankTransfer+"</li>"+
+        "</ul>"+
       "</div>"+
       "<div class='footer'>"+
         "<button class='action-btn previous'>"+previous+"</button>"+
         "<button class='action-btn green-action-btn confirm'>"+confirm+"</button>"+
       "</div>"+
     "</div>");
-    $(result).find('.previous').click(function() {
+    $(self.tab).find('.previous').click(function() {
       $(self).trigger('previous');
     });
-    $(result).find('.confirm').click(function() {
+    $(self.tab).find('.confirm').click(function() {
       $(self).trigger('confirm');
     });
-    return result;
+    $(self.tab).find('li').click(function() {
+      if ($(this).hasClass('list-group-item-active')) $(this).removeClass('list-group-item-active');
+      else $(this).addClass('list-group-item-active');
+    });
+    $(self.tab).find('.iban').click(function(e) {
+      e.stopPropagation();
+    });
+    return self.tab;
   }
 };
