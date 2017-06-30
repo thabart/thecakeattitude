3c89eb44a3422b031f57ef705cf5f6d9658c8aa5
thabart <habarthierry@hotmail.fr>
2017-06-30 18:05:59 +0200

    Ticket 10 : Add last women haircut

--- game/public/Constants.js
+++ game/public/Constants.js
@@ -438,7 +438,41 @@ var Constants = {
 							name: 'fifthBlackWomen',
 							overviewImage: '/styles/heads/women/fifth-hc-black-overview.png',
 							paddingY : -20,
-							paddingX : -1
+							paddingX : -1,
+							animations: [
+								{
+									name: 'down',
+									tiles: [32]
+								},
+								{
+									name: 'bottomRight',
+									tiles: [33]
+								},
+								{
+									name: 'right',
+									tiles: [34]
+								},
+								{
+									name: 'topRight',
+									tiles: [35]
+								},
+								{
+									name: 'top',
+									tiles: [36]
+								},
+								{
+									name: 'topLeft',
+									tiles: [37]
+								},
+								{
+									name: 'left',
+									tiles: [38]
+								},
+								{
+									name: 'bottomLeft',
+									tiles: [39]
+								}
+							]
 						}
 					]
 				}
