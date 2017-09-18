993d9b26920dbb0304899c70b49d6883960720f3
thabart <habarthierry@hotmail.fr>
2017-09-18 17:16:13 +0200

    RB changes

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/*
 import App from "./App";
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-import Footer from './Footer';*/
+import Footer from './Footer';
 
 ReactDOM.render(
-    <div></div>,
+    <I18nextProvider
+       i18n={ i18n }
+       initialI18nStore={window.initialI18nStore}
+       initialLanguage={window.initialLanguage}
+     >
+      <App />
+    </I18nextProvider>,
     document.getElementById('root')
 );
