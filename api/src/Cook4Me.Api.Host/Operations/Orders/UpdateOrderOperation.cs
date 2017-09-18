11fb22133c7d9f73a4c9c1d7c3774a18073edc18
thabart <habarthierry@hotmail.fr>
2017-09-18 20:59:09 +0200

    Fix URL

--- api/src/Cook4Me.Api.Host/Operations/Orders/UpdateOrderOperation.cs
+++ api/src/Cook4Me.Api.Host/Operations/Orders/UpdateOrderOperation.cs
@@ -170,8 +170,8 @@ namespace Cook4Me.Api.Host.Operations.Orders
                        }
                     }
                 },
-                CancelUrl = $"{_settingsProvider.GetBaseWebsite()}/orders/{result.Order.Id}/acceptpayment",
-                ReturnUrl = $"{_settingsProvider.GetBaseWebsite()}/orders/{result.Order.Id}/cancelpayment"
+                CancelUrl = $"{_settingsProvider.GetBaseWebsite()}/orders/{result.Order.Id}/cancelpayment",
+                ReturnUrl = $"{_settingsProvider.GetBaseWebsite()}/orders/{result.Order.Id}/acceptpayment"
             });
         }
     }
