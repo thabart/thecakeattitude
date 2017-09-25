1ada19237402897d3ec8fcb0fd489c7d3fe7aa9b
thabart <habarthierry@hotmail.fr>
2017-09-25 20:26:14 +0200

    Display payment details

--- web/src/Payment.js
+++ web/src/Payment.js
@@ -9,7 +9,8 @@ class Payment extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			isLoading: false
+			isLoading: false,
+			paymentDetails: {}
 		};
 	}
 
@@ -17,14 +18,14 @@ class Payment extends Component {
 		var self = this;
 		self.setState({
 			isLoading: true
-		});		
+		});
 		const {t} = self.props;
       	var orderId = self.props.match.params.id;
       	OrdersService.getPaymentDetails(orderId).then(function(paymentDetails) {
             self.setState({
-            	isLoading: false
+            	isLoading: false,
+							paymentDetails: paymentDetails
             });
-            console.log(paymentDetails);
       	}).catch(function(e) {
             var errorMsg = t('orderPaymentDetailsError');
             if (e.responseJSON && e.responseJSON.error_description) {
@@ -32,7 +33,8 @@ class Payment extends Component {
             }
 
             self.setState({
-            	isLoading: false
+            	isLoading: false,
+							paymentDetails: {}
             });
             ApplicationStore.sendMessage({
               message: errorMsg,
@@ -44,13 +46,52 @@ class Payment extends Component {
 
 	render() { // Display the component.
 		var self = this;
-		const {t} = self.props;
+		const { t } = self.props;
+		var products = [];
+		if (self.state.paymentDetails && self.state.paymentDetails.items && self.state.paymentDetails.items.length > 0) {
+			self.state.paymentDetails.items.forEach(function(item) {
+				products.push((<li className="list-group-item">
+					<div className="col-md-9">
+						<h4>{item.name}</h4>
+						<p>
+							{ t('quantity').replace('{0}', item.quantity)} <br />
+							{ t('pricePerUnit').replace('{0}', '€ ' + item.price) }
+						</p>
+					</div>
+					<div className="col-md-3">
+						<h4>€ {item.quantity * item.price}</h4>
+					</div>
+				</li>));
+			});
+		}
+
 		return ( <MainLayout isHeaderDisplayed={true} isFooterDisplayed={true}>
 			<div className={ !self.state.isLoading ? "hidden" : "container" }><i className="fa fa-spinner fa-spin"></i></div>
 			<div className={ self.state.isLoading ? "hidden": "container" }>
 				<h2>{t('paymentDetailsTitle')}</h2>
 				<section className="section" style={{padding: "10px", marginTop: "10px"}}>
-
+					<div className="row">
+						<div className="col-md-6">
+			      	{ products.length === 0 ? (<span>{t('noProducts')}</span>) : (
+							<div className="list-group-default">
+								<li className="list-group-item">
+									<div className="col-md-3 offset-md-9">{t('price')}</div>
+								</li>
+			          {products}
+							</div>
+							) }
+						</div>
+						<div className="col-md-6">
+							<section>
+			          <h5>{t('status')}</h5>
+			          <span className="badge badge-default">{t('status_' + self.state.paymentDetails.state)}</span>
+							</section>
+							<section>
+								<h5>{t('shippingPrice').replace('{0}', self.state.paymentDetails.shipping_price)}</h5>
+							</section>
+						</div>
+					</div>
+					<h4>{t('totalPrice').replace('{0}', self.state.paymentDetails.total)}</h4>
 				</section>
 			</div>
 		</MainLayout>);
