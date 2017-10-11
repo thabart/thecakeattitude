fbc43b173d0ec114433c3ec35fb3ac5bbf0ed10b
thabart <habarthierry@hotmail.fr>
2017-10-11 19:11:19 +0200

    Add first worker role

---
+++ api/src/OrderWorkerRole/WorkerRole.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderWorkerRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+
+        public override void Run()
+        {
+            Trace.TraceInformation("OrderWorkerRole is running");
+            try
+            {
+                this.RunAsync(this.cancellationTokenSource.Token).Wait();
+            }
+            finally
+            {
+                this.runCompleteEvent.Set();
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Définir le nombre maximum de connexions simultanées
+            ServicePointManager.DefaultConnectionLimit = 12;
+            // Pour plus d'informations sur la gestion des modifications de configuration
+            // consultez la rubrique MSDN à l'adresse https://go.microsoft.com/fwlink/?LinkId=166357.
+            bool result = base.OnStart();
+            Trace.TraceInformation("OrderWorkerRole has been started");
+
+            return result;
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("OrderWorkerRole is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("WorkerRole1 has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Remplacez le texte suivant par votre propre logique.
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Trace.TraceInformation("Working");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
